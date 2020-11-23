8fb15a593af1c676de61d00db3860117e5693d2d
David <33319760+techtalkcorner@users.noreply.github.com>
2020-11-24 07:14:23 +1000

    Update GetAzureDataFactoryMSI.ps1

--- Scripts/PowerShell/GetAzureDataFactoryMSI.ps1
+++ Scripts/PowerShell/GetAzureDataFactoryMSI.ps1
@@ -1,4 +1,4 @@
-﻿# This scripts returns the Azure Data Factory MSI to give access to your service
+# This scripts returns the Azure Data Factory managed identity to give access to other Azure service
 # Install module Azure if it is not available
 # Install-module Az
 # Pre-requisite, connect to your Azure account
@@ -11,5 +11,9 @@ $PrincipalId= (Get-AzDataFactoryV2 -ResourceGroupName $ResourceGroupName  -Name 
 $ApplicationId = Get-AzADServicePrincipal -ObjectId $PrincipalId
 $ApplicationId =($ApplicationId).ApplicationId
 
+write-host "The application Id is: " $ApplicationId
+write-host "The object Id is: " $PrincipalId
+
 # Copy the following user and give it access in Azure Analysis Services
+write-host "If giving access this service to your Azure Analysis Services, copy the following string:"
 Write-Host "app:$ApplicationId@$TenantId"
